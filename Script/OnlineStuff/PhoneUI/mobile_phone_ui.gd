7de5a275259ff17e23ba7198ee2db8196b3edb65
Rahman <habibur.rahman@accelbyte.net>
2026-05-10 15:19:21 +0700

    fix: connect close phone UI button

--- Script/OnlineStuff/PhoneUI/mobile_phone_ui.gd
+++ Script/OnlineStuff/PhoneUI/mobile_phone_ui.gd
@@ -4,12 +4,16 @@ class_name MobilePhoneUI
 
 func open():
 	visible = true
-	get_tree().paused = true
+	#get_tree().paused = false
 
 func close():
 	visible = false
-	get_tree().paused = false
+	#get_tree().paused = true
+	
+func _process(delta):
+	#detect the input of open_phone_input
+	if Input.is_action_just_pressed("open_phone_ui"):
+		open() 
 
-##deactivate this, only for testing purpose
-#func _ready() -> void:
-	#
+func _on_close_button_button_up() -> void:
+	close()
